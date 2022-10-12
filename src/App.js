81be30e7ed3231b579e6af98a51478bebf8a7e55
Igor Miljenović <igor.miljenovic@gmail.com>
2022-10-12 09:23:50 +0200

    api namjesten

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Gradovi from './gradovi.json'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-function App(props) {
+function App(response) {
 
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -30,6 +30,23 @@ function App(props) {
 
   let findfavorite = Gradovi.filter(val => favorite.includes(val.city));
 
+  let grad = Gradovi.filter(val => searchTerm.includes(val.city));
+
+  const [weatherData, setWeatherData] = useState([{}]);
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch('https://api.open-meteo.com/v1/forecast?latitude=' + grad.map(val => {
+        return (val.lat)}) + '&longitude=' + grad.map(val => {return(val.lng)}) + '&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,weathercode,pressure_msl,surface_pressure,cloudcover,windspeed_10m,windspeed_80m,windspeed_120m,windspeed_180m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm').then(
+       response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+          console.log(weatherData)
+        }
+      )
+    }
+  }
 
   return (
     <div className="App">
@@ -40,6 +57,7 @@ function App(props) {
         placeholder="Pretrazi gradove u HR"
         onChange={event => {setSearchTerm(event.target.value)}}
         value={searchTerm}
+        onKeyPress={getWeather}
         />
 
         <button type="submit" class="searchButton">
@@ -48,7 +66,7 @@ function App(props) {
       </div>
       <div className="search-res">
       {Gradovi.filter((val) => {
-        if (searchTerm == "") {
+        if (searchTerm === "") {
           return val
         } else if (val.city.toLowerCase().includes(searchTerm.toLowerCase())) {
           return val
@@ -63,12 +81,12 @@ function App(props) {
                </ul>
       })}
       </div>
-      <div className="favorite__list">
-        <h2>favorite recipes</h2>
+      <div className="favorite_list">
+        <h2>Favorites</h2>
           {findfavorite.map(val => {
             return (
               <div key={val.city} className="favs">
-              <h2 className="">{val.city}</h2>
+              <h4 className="">{val.city}</h4>
 
               <button onClick={() => removeFavorite(val.city)}>
               remove favorite
@@ -76,6 +94,11 @@ function App(props) {
               </div>
             );
           })}
+          </div>
+          <div className="prognoza">
+            <p>{weatherData.longitude}</p>
+            <p>{weatherData.latitude}</p>
+            <p>{weatherData.elevation}</p>
           </div>
     </div>
   );
